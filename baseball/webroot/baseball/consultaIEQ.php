78f22faf084dfdd4590733e584ffe09009a0dbce
Carlo Baldacci <papabalda@gmail.com>
2011-10-06 21:03:15 -0430

    Dos consultas

--- baseball/webroot/baseball/consultaIEQ.php
+++ baseball/webroot/baseball/consultaIEQ.php
@@ -4,17 +4,30 @@
 		include "config.php";
 
 	$Nombre = pg_escape_string (trim($_POST['nombre']));
-	$Apellido = pg_escape_string(trim($_POST['apellido']));
-	$Numero = trim($_POST['numero']);
-	$Tipo = pg_escape_string(trim($_POST['tipo']));
-	$Posicion = trim($_POST['posicion']);
-	$Fecha = trim($_POST['fecha']);
-	$Lugar = pg_escape_string(trim($_POST['lugar']));
-	$Peso = trim($_POST['peso']);
-	$Altura = trim($_POST['altura']);
-	$Equipo = pg_escape_string(trim($_POST['equipo']));
+	$Cantidad = trim($_POST['cantidad']);
+	$Manager = pg_escape_string(trim($_POST['manager']));
+	$Ffundacion = trim($_POST['ffundacion']);
+	$ciudad = pg_escape_string(trim($_POST['ciudad']));
+	$estadio = trim($_POST['estadio']);
 
         $query = "INSERT INTO \"jugador\" (\"Nombre_jugador\",\"Apellido_jugador\",\"Nro_jugador\",\"Tipo_jugador\",\"Posición_jugador\",\"Fecha_nacimiento\",\"lugar_nacimiento\",\"peso\" ,\"altura\",\"equipo_id\") SELECT '{$Nombre}', '{$Apellido}','{$Numero}','{$Tipo}','{$Posicion}','{$Fecha}','{$Lugar}','{$Peso}','{$Altura}', \"Equipo\".\"id\" FROM \"Equipo\" WHERE \"Equipo\".\"nombre\" = '{$Equipo}'";
+
+INSERT INTO "Equipo" ("nombre", "año de fundación", "ciudad", "estado", "estadio principal")
+SELECT "Datos"."nombre", "Datos"."año de fundación", "Datos"."ciudad", "Datos"."estado", "Estadio"."id"
+FROM
+        "Estadio",
+        (
+                VALUES
+                ('Águilas del Zulia'        , 1968, 'Maracaibo'     , 'Zulia'           , 'Luis Aparicio'             ),
+                ('Bravos de Margarita'      , 2007, 'Porlamar'      , 'Nueva Esparta'   , 'Nueva Esparta'             ),
+                ('Cardenales de Lara'       , 1942, 'Barquisimeto'  , 'Lara'            , 'Antonio Herrera Gutiérrez' ),
+                ('Caribes de Anzoátegui'    , 1987, 'Puerto La Cruz', 'Anzoátegui'      , 'Alfonso "Chico" Carrasquel'),
+                ('Leones del Caracas'       , 1952, 'Caracas'       , 'Distrito Capital', 'Universitario de Caracas'  ),
+                ('Navegantes del Magallanes', 1917, 'Valencia'      , 'Carabobo'        , 'José Bernardo Pérez'       ),
+                ('Tiburones de La Guaira'   , 1962, 'La Guaira'     , 'Vargas'          , 'Universitario'             ),
+                ('Tigres de Aragua'         , 1965, 'Maracay'       , 'Aragua'          , 'José Pérez Colmenares'     )
+        ) as "Datos" ("nombre", "año de fundación", "ciudad", "estado", "nombre del estadio principal")
+WHERE "Datos"."nombre del estadio principal" = "Estadio"."nombre";
 
 	
 
--- baseball/webroot/baseball/consultaIJ.php
+++ baseball/webroot/baseball/consultaIJ.php
@@ -3,19 +3,15 @@
 	error_reporting( E_ALL );
 		include "config.php";
 
-	$Nombre = pg_escape_string (trim($_POST['nombre']));
-	$Apellido = pg_escape_string(trim($_POST['apellido']));
-	$Numero = trim($_POST['numero']);
-	$Tipo = pg_escape_string(trim($_POST['tipo']));
-	$Posicion = trim($_POST['posicion']);
-	$Fecha = trim($_POST['fecha']);
-	$Lugar = pg_escape_string(trim($_POST['lugar']));
-	$Peso = trim($_POST['peso']);
-	$Altura = trim($_POST['altura']);
-	$Equipo = pg_escape_string(trim($_POST['equipo']));
-
-        $query = "INSERT INTO \"jugador\" (\"Nombre_jugador\",\"Apellido_jugador\",\"Nro_jugador\",\"Tipo_jugador\",\"Posición_jugador\",\"Fecha_nacimiento\",\"lugar_nacimiento\",\"peso\" ,\"altura\",\"equipo_id\") SELECT '{$Nombre}', '{$Apellido}','{$Numero}','{$Tipo}','{$Posicion}','{$Fecha}','{$Lugar}','{$Peso}','{$Altura}', \"Equipo\".\"id\" FROM \"Equipo\" WHERE \"Equipo\".\"nombre\" = '{$Equipo}'";
-
+	$Fecha = pg_escape_string (trim($_POST['fecha']));
+	$Hora = pg_escape_string(trim($_POST['hora']));
+	$Equipo1 = trim($_POST['equipo1']);
+	$Equipo2 = pg_escape_string(trim($_POST['equipo2']));
+	$Estadio = trim($_POST['estadio']);
+	$query = "SELECT * FROM JUEGA";
+/*
+        $query = "INSERT INTO \"Juega\" (\"Fecha\",\"Hora_de_inicio\",\"equipo1_id\",\"\",\"equipo2_id\",\"estadio_id\", SELECT '{$Nombre}', '{$Apellido}','{$Numero}','{$Tipo}','{$Posicion}','{$Fecha}','{$Lugar}','{$Peso}','{$Altura}', \"Equipo\".\"id\" FROM \"Equipo\" WHERE \"Equipo\".\"nombre\" = '{$Equipo}'";
+*/
 	
 
 	$result = pg_query($dbconn, $query);

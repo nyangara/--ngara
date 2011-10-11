03fe00cfa0850dc9a5adeb3ce956a9a1eef20aa1
Manuel Gómez <targen@gmail.com>
2011-10-10 22:44:21 -0430

    Arreglito a cargas y mejoras a Makefiles

--- projects/TriplePlay/database/load.sql
+++ projects/TriplePlay/database/load.sql
@@ -26,7 +26,7 @@ FROM
                 ('Bravos de Margarita'      , 2007, 'Porlamar'      , 'Nueva Esparta'   , 'Nueva Esparta'             ),
                 ('Cardenales de Lara'       , 1942, 'Barquisimeto'  , 'Lara'            , 'Antonio Herrera Gutiérrez' ),
                 ('Caribes de Anzoátegui'    , 1987, 'Puerto La Cruz', 'Anzoátegui'      , 'Alfonso "Chico" Carrasquel'),
-                ('Leones del Caracas'       , 1952, 'Caracas'       , 'Distrito Capital', 'Universitario de Caracas'  ),
+                ('Leones del Caracas'       , 1952, 'Caracas'       , 'Distrito Capital', 'Universitario'             ),
                 ('Navegantes del Magallanes', 1917, 'Valencia'      , 'Carabobo'        , 'José Bernardo Pérez'       ),
                 ('Tiburones de La Guaira'   , 1962, 'La Guaira'     , 'Vargas'          , 'Universitario'             ),
                 ('Tigres de Aragua'         , 1965, 'Maracay'       , 'Aragua'          , 'José Pérez Colmenares'     )

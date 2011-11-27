5df3b6ce2ba2d7f58c6367a2d56d5d183aaa7a03
Manuel Gómez <targen@gmail.com>
2011-11-27 17:27:26 -0430

    Función arreglada con nuevos datos del usuario

--- projects/Fantasy/database/functions.sql
+++ projects/Fantasy/database/functions.sql
@@ -22,10 +22,11 @@ $BODY$;
 
 -- TODO: manejar errores (parámetros nulos, usuario ya existente, etc)
 CREATE OR REPLACE FUNCTION "Fantasy"."registrar"(
-        IN      "parámetro: username"           text,
-        IN      "parámetro: nombre completo"    text,
-        IN      "parámetro: password"           text,
-        IN      "parámetro: es administrador"   boolean
+        IN      "parámetro: username"                   text,
+        IN      "parámetro: nombre completo"            text,
+        IN      "parámetro: dirección de e-mail"        text,
+        IN      "parámetro: es administrador"           boolean,
+        IN      "parámetro: password"                   text
 ) RETURNS void
         VOLATILE
         STRICT
@@ -36,10 +37,12 @@ AS $BODY$
                 INSERT INTO "Fantasy"."Usuario" (
                         "username",
                         "nombre completo",
+                        "dirección de e-mail",
                         "es administrador"
                 ) VALUES (
                         "parámetro: username",
                         "parámetro: nombre completo",
+                        "parámetro: dirección de e-mail",
                         "parámetro: es administrador"
                 );
 

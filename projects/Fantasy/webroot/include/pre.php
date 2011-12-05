5e15a37bbf7081e5f9085270ccb7caf000c3ddb4
Manuel Gómez <targen@gmail.com>
2011-12-05 01:44:13 -0430

    Arreglo al estilo que hizo Daniel

--- projects/Fantasy/webroot/include/pre.php
+++ projects/Fantasy/webroot/include/pre.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Liga Fantástica</title>
+    <title>Liga Fantabulosa</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="keywords"    content="fantasy, beisbol venezolano, liga venezolana de beisbol profesional, lvbp"/>
     <meta name="description" content="Fantasy de la liga venezolana de beisbol profesional."/>

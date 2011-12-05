e08a023710ba553b68f0bc7b7bdc50dcfbfeab92
Manuel Gómez <targen@gmail.com>
2011-12-04 23:58:26 -0430

    Cambios de Daniel integrados

--- projects/Fantasy/webroot/include/pre.php
+++ projects/Fantasy/webroot/include/pre.php
@@ -1,28 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-                <title>Liga Fantástica</title>
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <meta name="keywords"    content="fantasy, beisbol venezolano, liga venezolana de beisbol profesional, lvbp" />
-                <meta name="description" content="Fantasy de la liga venezolana de beisbol profesional." />
-                <link rel="stylesheet" href="static/styles/main_style.css" type="text/css" />
-                <link rel="Shortcut Icon" href="static/images/favicon.ico" />
-        </head>
-        <body id="<?php echo basename($_SERVER['SCRIPT_NAME'], '.php') ?>">
-                <div id="marquee">
-                    <div><span>Novedades:</span> <marquee behavior="scroll" direction="left" scrollamount="3">Esto es un marquee de prueba.</marquee></div>
-                </div>
-                <div id="wrapper">
-                        <div id="header">
-                                <div id="logo">
-                                        <img src="static/images/LogoGrande.png" alt="logo" width="48" height="48"/>
-                                        <h1><a href="index.php">Liga Fantástica</a></h1>
-                                </div>
-                                <div id="navigation">
-                                        <ul>
+  <head>
+    <title>Liga Fantástica</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="keywords"    content="fantasy, beisbol venezolano, liga venezolana de beisbol profesional, lvbp"/>
+    <meta name="description" content="Fantasy de la liga venezolana de beisbol profesional."/>
+    <link rel="stylesheet" href="static/styles/main_style.css" type="text/css"/>
+    <link rel="Shortcut Icon" href="static/images/favicon.ico"/>
+  </head>
+  <body id="<?php echo basename($_SERVER['SCRIPT_NAME'], '.php') ?>">
+    <div id="wrapper">
+      <div id="header">
+        <div id="logo">
+          <img src="static/images/LogoGrande.png" alt="logo" width="48" height="48"/>
+          <h1><a href="index.php">Liga Fantástica</a></h1>
+        </div>
+        <div id="login">
+          <form id="loginform" action="login" method="post">
+            <input type="text"     name="username" value="Nombre de usuario" onclick="if (value == 'Nombre de usuario') { value = ''; }"/>
+            <input type="password" name="password" value="Contraseña"        onclick="if (value == 'Contraseña'       ) { value = ''; }"/>
+            <input type="submit"   name="Login"    value="Acceder"/>
+          </form>
+          <form action="register">
+            <input name="Login" type="submit" value="Regístrate"/>
+          </form>
+        </div>
+        <div id="updates">
+          <span>
+            <strong>Novedades:</strong>
+            <marquee behavior="scroll" direction="left" scrollamount="3">Esto es un marquee de prueba.</marquee>
+          </span>
+        </div>
+        <div id="navigation">
+          <ul>
 <?php
         $vs = array(
-                'Inicio'      => 'index',
                 'Noticias'    => 'noticias',
                 'Jugadores'   => 'jugadores',
                 'Equipos'     => 'equipos',
@@ -33,13 +45,13 @@
         );
 
         foreach ($vs as $v => $f) {
-                $on = '';
-                if ($f == basename($_SERVER['SCRIPT_NAME'], '.php')) $on = ' class="on"';
 ?>
-                                                <li<?php echo $on; ?>><a href="<?php echo $f . ".php"; ?>"><?php echo $v; ?></a></li>
+            <li<?php echo ($f == basename($_SERVER['SCRIPT_NAME'], '.php')) ? ' class="on"' : ''; ?>>
+              <a href="<?php echo $f . ".php"; ?>"><?php echo $v; ?></a>
+            </li>
 <?php   } ?>
-                                        </ul>
-                                </div>
-                        </div>
-                        <div id="content">
-                                <div id="main">
+          </ul>
+        </div>
+      </div>
+      <div id="content">
+        <div id="main">
--- projects/Fantasy/webroot/index.php
+++ projects/Fantasy/webroot/index.php
@@ -5,53 +5,45 @@
 
         require 'include/pre.php';
 ?>
-<!-- Starting slider... -->
-<script type="text/javascript" src="static/js/jquery.js"></script>
-<script type="text/javascript" src="static/js/s3SliderPacked.js"></script>
-<script type="text/javascript" src="static/js/s3Slider.js"></script>
-<script type="text/javascript">
-        $(document).ready(function() {
-                $('#slider').s3Slider({
-                        timeOut: 4000
-                });
-        });
-</script>
-
-<div id="slider">
-  <ul id="sliderContent">
-    <li class="sliderImage">
-    <a href=""><img src="static/images/slider/imagen1.jpg" alt="1" width="600" height="300" /></a>
-    <span class="left"><strong>Vive...</strong><br />La emoción de el béisbol...</span>
-    </li>
-    <li class="sliderImage">
-    <a href=""><img src="static/images/slider/imagen2.jpg" alt="2" width="600" height="300" /></a>
-    <span class="right"><strong>Diseña...</strong><br />El equipo de tus sueños...</span>
-    </li>
-    <li class="sliderImage">
-    <img src="static/images/slider/imagen3.jpg" alt="3" width="600" height="300" />
-    <span class="right"><strong>Juega...</strong><br />El Fantasy de la LPBV</span>
-    </li>
-    <div class="clear sliderImage"></div>
-  </ul>
+<h2>Noticias</h2>
+<form>
+        <input name="" type="text"/>
+        <input name="" type="submit" value="Buscar"/>
+</form>
+<?
+        foreach (UIFacade::retrieveAll('Contenido') as $c) {
+                if ($c->get('tipo') == 'noticia') {
+                        $id   = $c->get('id'           );
+                        $tags = $c->get('tags'         );
+                        $img  = $c->get('URL de imagen');
+                        if ($img and !filter_var($img, FILTER_VALIDATE_URL)) $img = 'static/images/contenido/' . $img;
+?>
+<div>
+        <div class="admin-options">
+                <form method="post" action="controller">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="hidden" name="goto" value="noticias"/>
+                        <button type="submit" name="action" value="contenido_remove">Eliminar</button>
+                </form>
+                <form method="post" action="contenido_update">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" name="contenido_update" value="Modificar"/>
+                </form>
+        </div>
+        <h3><?php echo $c->get('título'); ?></h3>
+        <h4><?php echo $c->get('fecha'); ?></h4>
+        <br/>
+<?php                   if ($img) { ?>
+        <img src="<?php echo $img; ?>"/>
+<?php                   } ?>
+        <?php echo $c->get('texto'); ?>
+<?php                   if ($tags) { ?>
+        <p><strong>Etiquetas:</strong> <?php echo $tags; ?></p>
+<?php                   } ?>
 </div>
-<!-- Slider ended. -->
+<?php
+                }
+        }
 
-<div id="login">
-  <div>
-    <form action="#">
-      <h2>Acceder</h2>
-      <p>Nombre de usuario: </p>
-      <p><input type="text" name="username" value=""/></p>
-      <p>Contraseña:</p>
-      <p><input type="password" name="password" value=""/></p>
-      <p><input name="Login" type="submit" value="Acceder"/></p>
-    </form>
-  </div>
-  <div>
-    <h2>¿Eres nuevo?</h2>
-    <form action="registro.php">
-      <input name="Register" type="submit" value="Crear cuenta"/>
-    </form>
-  </div>
-</div>
-<?php   require('include/post.html'); ?>
+        require 'include/post.html';
+?>
